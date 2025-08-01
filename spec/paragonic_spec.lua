60731c28aacd66a2ac973e55544ddcc6eac7483f
Simon Janes <simonjanes@fastmail.com>
2025-08-01 11:55:59 -0500

    wip: add command functionality tests with nlua TDD

--- spec/paragonic_spec.lua
+++ spec/paragonic_spec.lua
@@ -24,5 +24,66 @@ local function test_paragonic_setup()
     print("✓ All tests passed!")
 end
 
--- Run the test
-test_paragonic_setup() 
+-- Test that setup creates Neovim commands
+local function test_setup_creates_commands()
+    print("Testing that setup creates Neovim commands...")
+    
+    -- Load the module
+    local paragonic = require("paragonic")
+    
+    -- Track if nvim_create_user_command was called
+    local command_calls = 0
+    local original_create_command = vim.api.nvim_create_user_command
+    vim.api.nvim_create_user_command = function(name, callback, opts)
+        command_calls = command_calls + 1
+        print("  Created command: " .. name)
+    end
+    
+    -- Call setup
+    paragonic.setup()
+    
+    -- Restore original function
+    vim.api.nvim_create_user_command = original_create_command
+    
+    -- Assert that commands were created
+    assert(command_calls >= 3, "Should create at least 3 commands (ParagonicChat, ParagonicProjects, ParagonicConfig)")
+    
+    print("✓ Command creation test passed!")
+end
+
+-- Test that commands show appropriate messages
+local function test_commands_show_messages()
+    print("Testing that commands show appropriate messages...")
+    
+    -- Load the module
+    local paragonic = require("paragonic")
+    
+    -- Track notify calls
+    local notify_calls = {}
+    local original_notify = vim.notify
+    vim.notify = function(msg, level)
+        table.insert(notify_calls, {msg = msg, level = level})
+        print("  Notify: " .. msg)
+    end
+    
+    -- Call the command functions directly
+    paragonic.open_chat()
+    paragonic.open_projects()
+    paragonic.open_config()
+    
+    -- Restore original function
+    vim.notify = original_notify
+    
+    -- Assert that appropriate messages were shown
+    assert(#notify_calls >= 3, "Should show at least 3 notification messages")
+    assert(notify_calls[1].msg:find("not yet implemented"), "Chat should show not implemented message")
+    assert(notify_calls[2].msg:find("not yet implemented"), "Projects should show not implemented message")
+    assert(notify_calls[3].msg:find("not yet implemented"), "Config should show not implemented message")
+    
+    print("✓ Command message test passed!")
+end
+
+-- Run all tests
+test_paragonic_setup()
+test_setup_creates_commands()
+test_commands_show_messages() 
